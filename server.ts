99dbf2ee1a2c24aa88d0072e681777fc33b224b4
David Chapman <talk2dc@gmail.com>
2022-10-03 22:27:02 -0400

    Add endpoint for fetching specific program by id

--- server.ts
+++ server.ts
@@ -27,6 +27,18 @@ router.get("/api/programs", (ctx) => {
   ctx.response.body = programs;
 });
 
+router.get("/api/programs/:id", (ctx) => {
+  const id = ctx.params.id;
+
+  const program = programs.find((program) => program.id === id);
+
+  if (program) {
+    ctx.response.body = program;
+  } else {
+    throw new httpErrors.NotFound(`Program not found with id: ${id}`);
+  }
+});
+
 const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
